09ee5c04af282bdac1acfb179bf35d16194be24c
Claire <54175187+claire-loves-data@users.noreply.github.com>
2021-12-03 15:53:57 +0000

    removeold name

--- python/advent1.py
+++
@@ -1,8 +0,0 @@
-with open("input.txt") as fp:
-    countHigher = 0
-    prev = None
-    for line in fp:
-        if prev != None and int(line) > prev:
-            countHigher += 1
-        prev = int(line)
-print(countHigher)
